3522ec3f773e7fa3ba618d4eb99faa468fc949cf
alfianprada_ <alfian123d4@gmail.com>
2025-09-09 10:09:52 +0700

    menambahkan splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,89 @@ class _MyAppState extends State<MyApp> {
         cardColor: Colors.grey[900],
       ),
       themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      home: HomePage(toggleTheme: toggleTheme, isDarkMode: isDarkMode),
+      home: SplashScreen(toggleTheme: toggleTheme, isDarkMode: isDarkMode),
+    );
+  }
+}
+
+// ------------------- Splash Screen -------------------
+class SplashScreen extends StatefulWidget {
+  final VoidCallback toggleTheme;
+  final bool isDarkMode;
+
+  const SplashScreen({super.key, required this.toggleTheme, required this.isDarkMode});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward();
+
+    // Pindah ke HomePage setelah 3 detik
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => HomePage(
+            toggleTheme: widget.toggleTheme,
+            isDarkMode: widget.isDarkMode,
+          ),
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FadeTransition(
+        opacity: _animation,
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  "images/logo.png", // ganti dengan logo proyekmu
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Selamat Datang di Pet App!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
